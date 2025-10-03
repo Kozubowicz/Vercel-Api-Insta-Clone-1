ae6be4a8dbf1e8e45ec1520f0e0532f982123b20
Kozubowicz Piotr <kozubowiczpiotr@gmail.com>
2025-10-03 16:35:28 +0200

    update remove comment

--- api/removeComment.js
+++ api/removeComment.js
@@ -11,11 +11,6 @@ async function connectToDatabase() {
 }
 
 export default async function handler(req, res) {
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
   try {
     const { commentId, userId } = req.body || {};
 
